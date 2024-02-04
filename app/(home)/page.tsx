0a8378d7761327570a8ba7a3575c29527c13d10b
rogereichert <rogereichert@gmail.com>
2024-02-04 09:23:07 -0300

    feat: update all files

---
+++ app/(home)/page.tsx
@@ -0,0 +1,33 @@
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import Header from "../_components/header";
+import Search from "./_components/search"
+import BookingItem from "../_components/booking-item";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Roger!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+
+      <div className="px-4 mt-6">
+          <Search />
+      </div>
+
+      <div className="px-5 mt-6">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+        <BookingItem />
+      </div>
+
+    </div>
+  );
+}
